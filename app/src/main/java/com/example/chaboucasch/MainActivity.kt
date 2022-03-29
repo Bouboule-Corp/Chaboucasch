a9192d45fdfe2fbea2e24741c90b58f7845030e9
arthurboucard <arthur.boucard@hotmail.fr>
2022-03-29 16:16:48 +0200

    added empty main activity

--- app/src/main/java/com/example/chaboucasch/MainActivity.kt
+++ app/src/main/java/com/example/chaboucasch/MainActivity.kt
@@ -1,4 +1,11 @@
 package com.example.chaboucasch
 
-class MainActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
