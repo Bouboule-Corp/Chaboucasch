16905fca334cb149a917d7197c5718525e6c3a9d
Neyross <74605059+Neyross@users.noreply.github.com>
2022-03-31 14:45:40 +0200

    card are working

--- app/src/main/java/com/example/chaboucasch/fragments/GameFragment.kt
+++ app/src/main/java/com/example/chaboucasch/fragments/GameFragment.kt
@@ -6,31 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.chaboucasch.R
 
 
 class GameFragment : Fragment() {
     var score = 0
+    private var card : MutableList<Int> = IntRange(1, 52).step(1).toList().toMutableList()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_game, container, false)
-        view.findViewById<Button>(R.id.Tete_button).setOnClickListener () {
+        view.findViewById<Button>(R.id.Tete_button).setOnClickListener {
             updateScore(view, 1)
         }
-        view.findViewById<Button>(R.id.Pastete_Button).setOnClickListener(){
+        view.findViewById<Button>(R.id.Pastete_Button).setOnClickListener {
             updateScore(view, 1)
         }
-        view.findViewById<Button>(R.id.Rouge_Button).setOnClickListener(){
+        view.findViewById<Button>(R.id.Rouge_Button).setOnClickListener {
             updateScore(view, 2)
         }
-        view.findViewById<Button>(R.id.Noir_Button).setOnClickListener(){
+        view.findViewById<Button>(R.id.Noir_Button).setOnClickListener {
             updateScore(view, 2)
         }
         return view
     }
-    fun updateScore(view: View, nb: Int) {
+    private fun updateScore(view: View, nb: Int) {
         val textview = view.findViewById<TextView>(R.id.score)
+        val cardView = view.findViewById<ImageView>(R.id.card)
+        getCard(cardView)
         if (nb > 0) {
             score += nb
             textview.text = "Score : $score"
@@ -38,5 +42,13 @@ class GameFragment : Fragment() {
         else {
             score = 0
         }
+    }
+    private fun getCard(cardView: ImageView) {
+        if (card.isEmpty())
+            card = IntRange(1, 52).step(1).toList().toMutableList()
+        val index = (card.indices).random()
+        val cardNb = card.removeAt(index)
+        val dispCard = "card_$cardNb"
+        cardView.setImageResource(resources.getIdentifier(dispCard, "drawable", "com.example.chaboucasch"))
     }
 }
